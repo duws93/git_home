86eac103676863cdb482fad88b9f7df7db0f697a
duws93 <40554075+duws93@users.noreply.github.com>
2019-11-26 16:06:45 +0800

    Update 4.groovy

--- simple_pipeline/4.groovy
+++ simple_pipeline/4.groovy
@@ -28,23 +28,32 @@ pipeline{
     }
   }
   post{
-         success{
-            emailext(
-               subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-               body: """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    success{
+      emailext(
+        subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+        body: """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
 <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
-               to: "wenshu.du.ext@nokia-sbell.com",
-               from: "wenshu.du.ext@nokia-sbell.com"
-            )
-         }
-         failure{
-            emailext (
-                subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-                body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        to: "wenshu.du.ext@nokia-sbell.com",
+        from: "wenshu.du.ext@nokia-sbell.com"
+      )
+    }
+    failure{
+      emailext (
+        subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+        body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
 <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
-                to: "wenshu.du.ext@nokia-sbell.com",
-                from: "wenshu.du.ext@nokia-sbell.com"
-            )
-         }
+        to: "wenshu.du.ext@nokia-sbell.com",
+        from: "wenshu.du.ext@nokia-sbell.com"
+      )
+    }
+    aborted{
+      emailext (
+        subject: "ABORTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+        body: """<p>ABORTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+<p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
+        to: "wenshu.du.ext@nokia-sbell.com",
+        from: "wenshu.du.ext@nokia-sbell.com"
+      )
+    }
   }
 }

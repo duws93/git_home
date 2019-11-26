7466b7d598226f468788af6c219178c66e6dddd7
duws93 <40554075+duws93@users.noreply.github.com>
2019-11-26 13:36:34 +0800

    Update 2.groovy

--- simple_pipeline/2.groovy
+++ simple_pipeline/2.groovy
@@ -1,1 +1,25 @@
-pipeline
+#!groovy
+pipeline{
+  environment{
+    foo = credentials("foocred")
+  }
+  
+  agent { label "duws-3" }
+  
+  stages{
+    stage("foo"){
+      steps{
+        sh 'echo "foo is $foo"'
+        sh 'echo "foo_user is $foo_user"'
+        sh 'echo "foo_psw is $foo_pws"'
+        
+        dir("combined"){
+          sh 'echo $foo > foo.txt'
+        }
+        sh 'echo $foo_pws > foo_psw.txt'
+        sh 'echo $foo_user > foo_user.txt'
+        archive "**/*.txt"
+      }
+    }
+  }
+}

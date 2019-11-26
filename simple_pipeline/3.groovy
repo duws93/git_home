0651222f7bf43ab32555cc7079b79f018d5a05cc
duws93 <40554075+duws93@users.noreply.github.com>
2019-11-26 14:38:25 +0800

    Update 3.groovy

--- simple_pipeline/3.groovy
+++ simple_pipeline/3.groovy
@@ -1,9 +1,19 @@
 #!groovy
 pipeline{
   agent{
+    label "duws-3"
     dockerfile{
       filename "Dockerfile.alternate"
       args "-v /tmp:/tmp -p 8000:8000"
+    }
+  }
+  
+  stages{
+    stage("foo"){
+      steps{
+        sh 'cat /hi-there'
+        sh 'echo "the answer is 42.5"'
+      }
     }
   }
 }

d7df445613232e29d1ff8c18c1838e0d8188a2b7
duws93 <40554075+duws93@users.noreply.github.com>
2019-11-26 10:41:56 +0800

    Update 1.groovy

--- simple_pipeline/1.groovy
+++ simple_pipeline/1.groovy
@@ -1,1 +1,28 @@
-wwww
+pipeline {
+  environment {
+    //环境变量和凭证检索
+    some_var = "some value"
+    cred1 = credential("cred1")
+    inbetween = "something in between"
+    cred2 = credential("cred2")
+    other_var = "${some_var}"
+  }
+  
+  agent any
+  
+  stages{
+    stage("foo"){
+      steps{
+        //环境变量未做mask处理
+        sh 'echo "some_var is $some_var"'
+        sh 'echo "inbetween is $inbetween"'
+        sh 'echo "other_var is $other_var"'
+        
+        //凭证变量在console中将被mask处理，但并不存档在文件中
+        sh 'echo $cred1 > cred1.txt'
+        sh 'echo $cred2 > cred2.txt'
+        archive "**/*.txt"
+      }
+    }
+  }
+}

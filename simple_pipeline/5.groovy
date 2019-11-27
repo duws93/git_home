4264829ddb06efabbe46a37d7e51c5043a1e4724
duws93 <40554075+duws93@users.noreply.github.com>
2019-11-27 10:37:37 +0800

    Update 5.groovy

--- simple_pipeline/5.groovy
+++ simple_pipeline/5.groovy
@@ -1,17 +1,19 @@
 #!groovy
 
 pipeline {
+  tools{
+    maven "apache-maven-3.6.3"
+    jdk "default"
+  }
   agent { label "duws-3"}
   stages{
     stage("foo"){
-      tools{
-        maven "apache-maven-3.6.3"
-        jdk "default"
-      }
       steps{
-        dir("/root/tools/hudson.tasks.Maven_MavenInstallation/apache-maven-3.6.3/bin"){
-          sh './mvn -v'
-          sh 'printenv | grep env'
+        dir("tmp"){
+          git changelog: false, poll: false, url: 'https://github.com/duws93/git_home.git', branch: 'master'
+          sh ' echo "M2_HOME: ${M2_HOME}"'
+          sh 'echo "JAVA_HOME: ${JAVA_HOME}"'
+          sh 'mvn clean verify -Dmaven.test.failure.ignore=true'
         }
       }
     }

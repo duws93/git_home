a1050fbddf4d27dc834c991e3cf9f298f546840b
duws93 <40554075+duws93@users.noreply.github.com>
2019-11-27 10:16:23 +0800

    Update 5.groovy

--- simple_pipeline/5.groovy
+++ simple_pipeline/5.groovy
@@ -4,11 +4,12 @@ pipeline {
   agent { label "duws-3"}
   stages{
     stage("foo"){
-      tools{
-        maven "apache-maven-3.6.3"
-      }
       steps{
-        echo "test maven"
+        dir("/root/workspace/tools"){
+          tools{
+            maven "apache-maven-3.6.3"
+          }
+        }
       }
     }
   }

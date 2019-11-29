387b0c835bd4ba276ac72f629c982bb99e11e9bb
duws93 <40554075+duws93@users.noreply.github.com>
2019-11-29 14:23:24 +0800

    Update 7.groovy

--- simple_pipeline/7.groovy
+++ simple_pipeline/7.groovy
@@ -1,2 +1,31 @@
 #!groovy
 pipeline{
+  agent {
+    label "duws-3"
+  }
+  stages{
+    stage('stash'){
+      agent{
+        label "duws-3"
+      }
+      steps{
+        writeFile file: "a.txt", text: "$BUILD_NUMBER"
+        stash name: "abc", include: "a.txt"
+      }
+    }
+    
+    stage("unstash"){
+      agent{
+        label "duws-2"
+      }
+      steps{
+        script{
+          unstash("abc")
+          def content = readFile("a.txt")
+          echo "${content}"
+        }
+      }
+    }
+  }
+}
+         

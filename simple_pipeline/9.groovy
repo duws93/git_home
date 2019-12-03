454a3126992744b8a7b6dd901573f352f4396a12
duws93 <40554075+duws93@users.noreply.github.com>
2019-12-03 13:16:05 +0800

    Update 9.groovy

--- simple_pipeline/9.groovy
+++ simple_pipeline/9.groovy
@@ -7,16 +7,16 @@ pipeline{
   }
   stages{
     stage('build'){
-      input{
-        message "press ok to continue"
-        submitter "admin,wenshud"
-        parameters{
-          string(name:'username',defaultValue: 'user',description:'username of the user pressing ok')
-        }
-      }
-      steps{
-        echo 'user: ${username} said ok'
-      }
+  input{
+    message "press ok to continue"
+    submitter "admin,wenshud"
+    parameters{
+      string(name:'username',defaultValue: 'user',description:'username of the user pressing ok')
+    }
+  }
+  steps{
+    echo 'user: ${username} said ok'
+  }
     }
   }
 }

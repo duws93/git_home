7b608b716491940f3f16f31675d5f2b19142e4bf
duws93 <40554075+duws93@users.noreply.github.com>
2019-12-03 10:37:09 +0800

    Update 9.groovy

--- simple_pipeline/9.groovy
+++ simple_pipeline/9.groovy
@@ -1,7 +1,23 @@
 #!groovy
 pipeline{
   agent{
-  label "duws-3"
-  customworkspace '/root/workspace/pipeline'
+    label "duws-3"
+    customworkspace '/root/workspace/pipeline'
   }
+  
+  stages{
+    stage('build'){
+      input{
+        message "press ok to continue"
+        submitter "admin,wenshud'
+        parameters{
+          strings(name:'username',defaultValue: 'user',description:'username of the user pressing ok')
+        }
+      }
+      steps{
+        echo 'user: ${username} said ok'
+      }
+    }
+  }
+}
   

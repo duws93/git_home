7a6336f606442d5ac5c0d370f505c8fbe58d5ea8
duws93 <40554075+duws93@users.noreply.github.com>
2019-12-04 15:38:58 +0800

    Update 1.groovy

--- simple_pipeline/docker/1.groovy
+++ simple_pipeline/docker/1.groovy
@@ -24,7 +24,7 @@ pipeline{
     stage('zabbix-server'){
       steps{
         script{
-          def server_container = server.run("--name zabbix-server-mysql -it --restart=always -e DB_SERVER_HOST='mysql-server' -e MYSQL_DATABASE='zabbix' -e MYSQL_USER='root' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' --link mysql-server:mysql -p 10051:10051 -d zabbix/zabbix-server-mysql:latest")
+          def server_container = docker.run("--name zabbix-server-mysql -it --restart=always -e DB_SERVER_HOST='mysql-server' -e MYSQL_DATABASE='zabbix' -e MYSQL_USER='root' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' --link mysql-server:mysql -p 10051:10051 -d zabbix/zabbix-server-mysql:latest")
           echo "zabbix-server container is running" 
         }
       }
@@ -33,7 +33,7 @@ pipeline{
     stage('zabbix-web'){
       steps{
         script{
-          def web_container = web.run("--name zabbix-web-nginx-mysql -it --restart=always -e DB_SERVER_HOST='mysql-server' -e MYSQL_DATABASE='zabbix' -e MYSQL_USER='root' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -p 8088:80 -d zabbix/zabbix-web-nginx-mysql:latest")
+          def web_container = docker.run("--name zabbix-web-nginx-mysql -it --restart=always -e DB_SERVER_HOST='mysql-server' -e MYSQL_DATABASE='zabbix' -e MYSQL_USER='root' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -p 8088:80 -d zabbix/zabbix-web-nginx-mysql:latest")
           echo "zabbix-web container is running"
         }
       }

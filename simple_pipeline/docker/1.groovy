a96cea9ee8e018380e6f0ddf4b8ddba502384aeb
duws93 <40554075+duws93@users.noreply.github.com>
2019-12-05 10:05:52 +0800

    Update 1.groovy

--- simple_pipeline/docker/1.groovy
+++ simple_pipeline/docker/1.groovy
@@ -12,6 +12,35 @@ pipeline{
   }
   
   stages{
+    //利用docker命令，执行dockerfile中的image生成
+    stage('mysql'){
+      stpes{
+        script{
+          def mysql = docker.build("mysql:5.7")
+          def mysql_container =  mysql.run("--name mysql-server -it --restart=always -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="root" -e MYSQL_PASSWORD="root" -e MYSQL_ROOT_PASSWORD="root" -v /data/mysql:/var/lib/mysql")
+          echo "mysql container is running"
+        }
+      }
+    }
+    stage("zabbix-server"){
+      steps{
+        script{
+          def server = docker.build("zabbix/zabbix-server-mysql:latest")
+          def server_container = server.run("--name zabbix-server-mysql -it --restart=always -e DB_SERVER_HOST="mysql-server" -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="root" -e MYSQL_PASSWORD="root" -e MYSQL_ROOT_PASSWORD="root" --link mysql-server:mysql -p 10051:10051")
+          echo "zabbix-server container is running"
+        }
+      }
+    }
+    stage('zabbix-web'){
+      steps{
+        script{
+          def web = docker.build("zabbix/zabbix-web-nginx-mysql:latest")
+          def web_container = web.run("--name zabbix-web-nginx-mysql -it --restart=always -e DB_SERVER_HOST='mysql-server' -e MYSQL_DATABASE='zabbix' -e MYSQL_USER='root' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -p 8088:80")
+          echo "zabbix-web container is running"
+        }
+      }
+    }
+    /*
     stage('mysql'){
       steps{
         sh 'docker run --name mysql-server -it --restart=always -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="root" -e MYSQL_PASSWORD="root" -e MYSQL_ROOT_PASSWORD="root" -v /data/mysql:/var/lib/mysql -d mysql:5.7'
@@ -30,17 +59,6 @@ pipeline{
       steps{
         sh 'docker run --name zabbix-web-nginx-mysql -it --restart=always -e DB_SERVER_HOST="mysql-server" -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="root" -e MYSQL_PASSWORD="root" -e MYSQL_ROOT_PASSWORD="root" --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -p 8088:80 -d zabbix/zabbix-web-nginx-mysql:latest'
         echo "zabbix-web container is running"
-      }
-    }
-    /*
-    //利用docker命令，执行dockerfile中的image生成
-    stage('zabbix-web'){
-      steps{
-        script{
-          def web = docker.build("zabbix/zabbix-web-nginx-mysql:latest")
-          def web_container = web.run("--name zabbix-web-nginx-mysql -it --restart=always -e DB_SERVER_HOST='mysql-server' -e MYSQL_DATABASE='zabbix' -e MYSQL_USER='root' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' --link mysql-server:mysql --link zabbix-server-mysql:zabbix-server -p 8088:80")
-          echo "zabbix-web container is running"
-        }
       }
     }
     */

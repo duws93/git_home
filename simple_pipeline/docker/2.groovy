f2c94c2ec6b1b8f4a645f5c4ed72222f84e477b5
duws93 <40554075+duws93@users.noreply.github.com>
2019-12-13 14:52:25 +0800

    Update 2.groovy

--- simple_pipeline/docker/2.groovy
+++ simple_pipeline/docker/2.groovy
@@ -6,14 +6,16 @@ podTemplate(label:label, cloud: 'kubernetes',
             containers:[containerTemplate(name: 'jnlp-slave', image: 'cnych/jenkins:v1')],
             volumes:[
               hostPathVolume(mountPath: '/usr/bin/docker', hostPath: '/usr/bin/docker'),
-              hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
-              hostPathVolume(mountPath: '/root/.kube', hostPath: '/root/.kube')])
-{
+              hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')]
+           ){
   node(label){
     stage('get a maven project'){
       container(label){
         stage('wait for exec check'){
-          sh 'kubectl get pod'
+          sh 'sleep 1'
+          sh 'echo $JAVA_HOME'
+          sh 'cat /etc/resolv.conf'
+          sh 'uname -a'
         }
       }
     }

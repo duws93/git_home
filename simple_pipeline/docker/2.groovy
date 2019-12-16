22697fccb51190fd68a24708f94beefd41dade1e
duws93 <40554075+duws93@users.noreply.github.com>
2019-12-16 10:51:32 +0800

    Update 2.groovy

--- simple_pipeline/docker/2.groovy
+++ simple_pipeline/docker/2.groovy
@@ -5,10 +5,10 @@ def label="jnlp-slave"
 podTemplate(label:label, cloud: 'kubernetes', 
             containers:[containerTemplate(name: 'jnlp-slave', image: 'cnych/jenkins:jnlp')],
             volumes:[
-//              hostPathVolume(mountPath: '/usr/bin/docker', hostPath: '/usr/bin/docker'),
+              hostPathVolume(mountPath: '/usr/bin/docker', hostPath: '/usr/bin/docker'),
               hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
               hostPathVolume(mountPath: '/home/jenkins/.kube', hostPath: '/root/.kube'),
-              hostPathVolume(mountPath: '/usr/local/bin/kubectl', hostPath: '/usr/local/bin/kubectl')
+              hostPathVolume(mountPath: '/usr/bin/kubectl', hostPath: '/usr/bin/kubectl')
             ]
            ){
   node(label){

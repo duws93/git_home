d87043eca69e619be93ef8c0a231952c6f6f2d36
duws93 <40554075+duws93@users.noreply.github.com>
2019-12-16 14:44:44 +0800

    Update 3.groovy

--- simple_pipeline/docker/3.groovy
+++ simple_pipeline/docker/3.groovy
@@ -5,7 +5,7 @@ podTemplate(label: label, cloud: 'kubernetes',containers: [
     containerTemplate(name: 'jnlp', image: 'cnych/jenkins:jnlp')
   ],
   volumes: [hostPathVolume(mountPath:'/var/run/docker.sock',hostPath:'/var/run/docker.sock'),
-            hostPathVolume(mountPath:' /home/jenkins/.kube',hostPath:'/root/.kube')]) {
+            hostPathVolume(mountPath:' /root/.kube',hostPath:'/root/.kube')]) {
     node(label) {
         stage('Get a Maven project') {
             container(label) {

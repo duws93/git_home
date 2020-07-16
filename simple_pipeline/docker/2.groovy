c4ba6b3bc9e84e47eb3a942b6e8164508ebfc3c8
duws93 <40554075+duws93@users.noreply.github.com>
2020-07-16 11:08:19 +0800

    Update 2.groovy

--- simple_pipeline/docker/2.groovy
+++ simple_pipeline/docker/2.groovy
@@ -2,7 +2,7 @@
   配置容器版Jenkins使用宿主机得kubectl命令
 */
 def label="jnlp-slave"
-podTemplate(label:label, cloud: 'kubernetes', 
+podTemplate(label:label, cloud: 'DCE4', 
             containers:[containerTemplate(name: 'jnlp-slave', image: 'cnych/jenkins:jnlp')],
             volumes:[hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
                      hostPathVolume(mountPath: '/home/jenkins/.kube', hostPath: '/root/.kube')
